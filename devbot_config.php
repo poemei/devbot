d758c169d63b07f8cbb5b4fc11fe93480eb8ccd0
Poe <poemei761@gmail.com>
2025-07-25 15:11:40 -0700

    Initial Push

---
+++ devbot_config.php
@@ -0,0 +1,10 @@
+<?php
+// Rictus Configuration File
+
+$db_host = 'localhost';
+$db_user = '';
+$db_pass = '';
+$db_name = '';
+
+$admin_id = 2; // DevBot the Scribe
+$log_dir  = __DIR__ . '/../logs'; // Adjust path if needed
